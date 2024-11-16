c3b063195d87d178aebf437687050e49507dbb23
JorgeSaicoski <jorgesaicoski@gmail.com>
2024-11-15 22:57:58 -0300

    routing working

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route
-            path="/dashboard"
+            path="/user/*"
             element={
               <ProtectedLoginRoute>
                 <UserRouter/>
